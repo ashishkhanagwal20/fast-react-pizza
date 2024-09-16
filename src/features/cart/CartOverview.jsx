8d302bc3c5515601564225905f042045488fa863
ashishkhanagwal20 <ashishstudy20@gmail.com>
2024-09-16 12:35:19 +0530

    Refactored total price calculation logic

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -5,7 +5,7 @@ import { formatCurrency } from '../../utils/helpers';
 function CartOverview() {
   const totalCartQuantity = useSelector(getTotalCartQuantity);
   const totalCartPrice = useSelector(getTotalCartPrice);
-  console.log('Cart', totalCartQuantity);
+  // console.log('Cart', totalCartQuantity);
   if (!totalCartQuantity) return null;
   return (
     <div className="flex items-center justify-between bg-stone-600 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
